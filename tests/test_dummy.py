c51a2c440487ceeeefc4b5bd77a3b71290e7d1f6
Madh93 <mhdez@protonmail.com>
2019-03-27 08:09:38 +0000

    Renamed to yadummy

--- tests/test_dummy.py
+++
@@ -1,4 +0,0 @@
-from dummy import dummy
-
-def test_say_hello():
-    assert dummy.say_hello() == 'Hello World!'
---
+++ tests/test_yadummy.py
@@ -0,0 +1,4 @@
+from yadummy import yadummy
+
+def test_say_hello():
+    assert yadummy.say_hello() == 'Hello World!'
