cf77f6119ed464fade5b47d6500a21442f36fe65
Madh93 <mhdez@protonmail.com>
2019-03-27 08:43:21 +0000

    Add setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+REQUIREMENTS = [
+    'requests'
+]
+
+TEST_REQUIREMENTS = [
+    'pytest'
+]
+
+setup(
+    name='yadummy',
+    version='0.0.1',
+    description="Yet Another Dummy Project",
+    author="Madh93",
+    author_email='mhdez@protonmail.com',
+    url='https://github.com/Madh93/yadummy',
+    packages=['yadummy'],
+    install_requires=REQUIREMENTS,
+    extras_require={'test': TEST_REQUIREMENTS},
+    scripts=['bin/yadummy'],
+    keywords=['dummy'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='pytest',
+)
